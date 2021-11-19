21b4e3b7207cc8914c39c226fd86920b40e84df1
Charles Blain <74670182+CharlyMittsu@users.noreply.github.com>
2021-11-19 09:03:41 +0100

    exo1

---
+++ tri.py
@@ -0,0 +1,35 @@
+# ______Déclaration
+import random
+stop = False
+manip = 0
+n = 0
+taille = int(input("entrez la taille du tableau : "))
+valeurMin = 0
+valeurMax = int(input("entrez une valeur max pour les éléments du tableau : "))
+
+
+# ______Création du tableau
+tableau = []
+for i in range(taille) :
+    tableau.append(random.randint(valeurMin,valeurMax))
+print ("taille du tableau : ",len(tableau))
+print (tableau)
+
+# ______Fonction
+def swap(x,y):
+    stock=tableau[x]
+    tableau[x]=tableau[y]
+    tableau[y]=stock
+
+def petit(x,y):
+    mini=tableau[x]
+    b=x
+    for i in range(x+1,y):
+        if tableau[i]<mini:
+            mini=tableau[i]
+            b=i
+    return b
+
+# ______Tri du tableau
+print(tableau)
+petit(0,taille)
